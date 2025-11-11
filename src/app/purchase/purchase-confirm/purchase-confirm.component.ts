7677b80303c0ae2b69a594c5dd303313cd628233
sawa <sawa@pascal.jp>
2025-11-11 16:05:22 +0900

    購入完了ページの改善

--- src/app/purchase/purchase-confirm/purchase-confirm.component.ts
+++ src/app/purchase/purchase-confirm/purchase-confirm.component.ts
@@ -109,11 +109,10 @@ export class PurchaseConfirmComponent implements OnInit {
       next: (res: any) => {
         this.loading = false;
         if (res.success) {
-          // カートをクリア
-          sessionStorage.removeItem('cart_items');
-
           console.log(res.message + res.sale_id);
-          this.router.navigate(['/purchase-confirmed']);
+          this.router
+            .navigate(['/purchase-confirmed'])
+            .then(() => window.scrollTo({ top: 0, behavior: 'smooth' }));
         } else {
           this.message = res.message || '購入に失敗しました。';
         }
