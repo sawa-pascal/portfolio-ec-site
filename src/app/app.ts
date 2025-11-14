e25cadd47bb03595d2a47c008270ae20153790a4
sawa <sawa@pascal.jp>
2025-11-14 16:56:04 +0900

    不要なコードの削除

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from "./footer/footer.component";
@@ -10,5 +10,5 @@ import { FooterComponent } from "./footer/footer.component";
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('portfolio');
+
 }
